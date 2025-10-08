86f072356335f7539979452fffcaf2d3194a82a9
Carlos Winicius <carlos.winicius055@gmail.com>
2025-10-08 01:11:39 -0300

    add pngs

--- src/main/java/br/upe/controller/ui/Main.java
+++ src/main/java/br/upe/controller/ui/Main.java
@@ -1,6 +1,5 @@
-package br.upe;
+package br.upe.controller.ui;
 
-import br.upe.controller.ui.HelloController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -11,10 +10,11 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloController.class.getResource("/ui/HelloWorld.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginScreenController.class.getResource("/ui/LoginScreen.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
