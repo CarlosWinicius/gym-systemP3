a645289f490e0d5e19152e5ff3e5bc9963360f3e
Carlos Winicius <carlos.winicius055@gmail.com>
2025-10-10 00:50:00 -0300

    aCommit

--- src/main/java/br/upe/controller/ui/SignInScreenController.java
+++ src/main/java/br/upe/controller/ui/SignInScreenController.java
@@ -2,6 +2,7 @@ package br.upe.controller.ui;
 
 import br.upe.controller.business.IUsuarioService;
 import br.upe.controller.business.UsuarioService;
+import br.upe.data.TipoUsuario;
 import br.upe.data.beans.Usuario;
 import br.upe.utils.SceneLoader;
 import javafx.application.Platform;
@@ -10,26 +11,27 @@ import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
-
-public class LoginScreenController {
+public class SignInScreenController {
+    @FXML
+    private TextField nameField;
 
     @FXML
     private TextField emailField;
 
     @FXML
-    private PasswordField passwordField;
+    private TextField passwordField;
 
     @FXML
-    private Button loginButton;
+    private TextField confirmPasswordField;
 
     @FXML
-    private Label createAccountLabel;
+    private Button SignInButton;
+
+    @FXML
+    private Label LoginAccountLabel;
 
     @FXML
     private Label errorMessage;
-
-    @FXML
-    private VBox rightPane; // ou o AnchorPane principal, dependendo do seu FXML
 
     private final IUsuarioService usuarioService = new UsuarioService();
 
@@ -38,37 +40,33 @@ public class LoginScreenController {
         Platform.runLater(() -> emailField.requestFocus());
 
         // adiciona eventos aos botões e labels
-        loginButton.setOnAction(e -> handleLogin());
-        createAccountLabel.setOnMouseClicked(this::handleCreateAccount);
-    }
+        SignInButton.setOnAction(e -> handleSignIn());
+//        LoginAccountLabel.setOnMouseClicked(this::handleCreateAccount);
 
-    private void handleLogin() {
+    }
+    private void handleSignIn() {
+        String nome = nameField.getText();
         String email = emailField.getText();
         String senha = passwordField.getText();
 
         try {
-            Usuario usuario = usuarioService.autenticarUsuario(email, senha);
+            Usuario usuario = usuarioService.cadastrarUsuario(nome,email,senha, TipoUsuario.COMUM);
 
             if (usuario != null) {
                 // Aqui você pode salvar o usuário em uma sessão
                 // e trocar de tela, por exemplo:
-                System.out.println("Login bem-sucedido! Usuário: " + usuario.getNome());
+                System.out.println("Cadastro bem-sucedido! Usuário: " + usuario.getNome());
                 // SceneLoader.loadScene("/org/upe/ui/telaInicio.fxml", "Home", rightPane);
             } else {
                 showError("Credenciais inválidas. Tente novamente.");
             }
         } catch (IllegalArgumentException e) {
-            showError("Erro: " + e.getMessage());
+            showError("Erro ao cadastrar: " + e.getMessage());
         } catch (Exception e) {
             showError("Erro inesperado: " + e.getMessage());
         }
     }
 
-    private void handleCreateAccount(MouseEvent e) {
-        // trocar tela para cadastro
-        System.out.println("Ir para tela de cadastro...");
-        SceneLoader.loadScene("/ui/SignInScreen.fxml", "Cadastro", rightPane);
-    }
 
     private void showError(String message) {
         if (errorMessage != null) {
