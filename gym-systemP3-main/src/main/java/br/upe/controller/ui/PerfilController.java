b8c34681ff2d45c89d1567d2dde548acfa82bcda
Maria Let <124286208+marialet14@users.noreply.github.com>
2025-10-16 15:33:46 -0300

    Update PerfilController.java

--- gym-systemP3-main/src/main/java/br/upe/controller/ui/PerfilController.java
+++ gym-systemP3-main/src/main/java/br/upe/controller/ui/PerfilController.java
@@ -1,56 +1,47 @@
 package br.upe.controller.ui;
-
+import br.upe.controller.business.CalculadoraIMC;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javafx.scene.input.MouseEvent;
 
 public class PerfilController extends BaseController {
 
-    @FXML private TextField txtNome;
-    @FXML private TextField txtAltura;
-    @FXML private TextField txtPeso;
-    @FXML private TextField txtGordura;
-    @FXML private TextField txtMassaMagra;
-    @FXML private TextField txtIMC;
-    @FXML private Label lblIMCStatus;
+    @FXML
+    private TextField Nome;
+    @FXML
+    private TextField Altura;
+    @FXML
+    private TextField Peso;
+    @FXML
+    private TextField PercentualGordura;
+    @FXML
+    private TextField PercentualMM;
+    @FXML
+    private TextField IMC;
+    @FXML
+    private Label CategoriaIMC;
 
     @FXML
     protected void initialize() {
-        // Exemplo de preenchimento inicial
-        txtNome.setText("Fulaninho de Tal da Silva");
-        txtAltura.setText("170");
-        txtPeso.setText("55.6");
-        txtGordura.setText("15.2");
-        txtMassaMagra.setText("70.8");
-        calcularIMC();
-    }
+        Nome.setText("Fulaninho de Tal da Silva");
+        Altura.setText("170");
+        Peso.setText("55.6");
+        PercentualGordura.setText("15.2");
+        PercentualMM.setText("70.8");
 
-    @FXML
-    protected void onSalvar(MouseEvent event) {
-        showAlert(Alert.AlertType.INFORMATION, "Perfil", "Informações salvas com sucesso!");
-    }
 
-    @FXML
-    protected void calcularIMC() {
-        try {
-            double peso = Double.parseDouble(txtPeso.getText());
-            double altura = Double.parseDouble(txtAltura.getText()) / 100.0; // cm → m
+        double altura = Double.parseDouble(Altura.getText());
+        double peso = Double.parseDouble(Peso.getText());
 
-            double imc = peso / (altura * altura);
-            txtIMC.setText(String.format("%.2f", imc));
 
-            if (imc < 18.5) {
-                lblIMCStatus.setText("abaixo do normal");
-            } else if (imc < 25) {
-                lblIMCStatus.setText("normal");
-            } else if (imc < 30) {
-                lblIMCStatus.setText("sobrepeso");
-            } else {
-                lblIMCStatus.setText("obesidade");
-            }
+        double imc = CalculadoraIMC.calcular(peso, altura);
 
-        } catch (NumberFormatException e) {
-            showAlert(Alert.AlertType.ERROR, "Erro", "Altura ou peso inválido.");
-        }
+
+        IMC.setText(String.format("%.2f", imc));
+
+
+        String classificacao = CalculadoraIMC.classificarImc(imc);
+        CategoriaIMC.setText(classificacao);
     }
 }
+
+
