e846681be8b20d7e9d828f9f2841ecbcef1153ad
marialet14 <marialeticia.mota@upe.br>
2025-12-12 19:55:14 -0300

    fix: PerfilScreen

--- src/main/java/br/upe/data/dao/UsuarioDAO.java
+++ src/main/java/br/upe/data/dao/UsuarioDAO.java
@@ -1,22 +1,78 @@
 package br.upe.data.dao;
 
 import br.upe.data.entities.Usuario;
+import br.upe.data.infra.ConnectionFactory;
 import br.upe.data.interfaces.IUsuarioRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.NoResultException;
 import jakarta.persistence.TypedQuery;
+
+import java.util.List;
 import java.util.Optional;
 
-public class UsuarioDAO extends GenericDAO<Usuario> implements IUsuarioRepository {
+public class UsuarioDAO implements IUsuarioRepository {
 
-    public UsuarioDAO() {
-        super(Usuario.class);
+    @Override
+    public Usuario salvar(Usuario usuario) {
+        EntityManager em = ConnectionFactory.getConnection();
+        try {
+            em.getTransaction().begin();
+            // Se não tem ID, é novo -> persist. Se tem ID, é atualização -> merge
+            if (usuario.getId() == null) {
+                em.persist(usuario);
+            } else {
+                usuario = em.merge(usuario);
+            }
+            em.getTransaction().commit();
+            return usuario;
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            throw e;
+        } finally {
+            em.close();
+        }
     }
 
+    @Override
+    public void editar(Usuario usuario) {
+        // No JPA, editar e salvar muitas vezes são a mesma operação (merge)
+        salvar(usuario);
+    }
+
+    @Override
+    public void deletar(int id) {
+        EntityManager em = ConnectionFactory.getConnection();
+        try {
+            em.getTransaction().begin();
+            Usuario usuario = em.find(Usuario.class, id);
+            if (usuario != null) {
+                em.remove(usuario);
+            }
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            throw e;
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public Optional<Usuario> buscarPorId(int id) {
+        EntityManager em = ConnectionFactory.getConnection();
+        try {
+            Usuario usuario = em.find(Usuario.class, id);
+            return Optional.ofNullable(usuario);
+        } finally {
+            em.close();
+        }
+    }
 
     @Override
     public Optional<Usuario> buscarPorEmail(String email) {
-        EntityManager em = getEntityManager(); // Método herdado do GenericDAO
+        EntityManager em = ConnectionFactory.getConnection();
         try {
             String jpql = "SELECT u FROM Usuario u WHERE u.email = :email";
             TypedQuery<Usuario> query = em.createQuery(jpql, Usuario.class);
@@ -29,4 +85,28 @@ public class UsuarioDAO extends GenericDAO<Usuario> implements IUsuarioRepositor
             em.close();
         }
     }
+
+    @Override
+    public List<Usuario> listarTodos() {
+        EntityManager em = ConnectionFactory.getConnection();
+        try {
+            return em.createQuery("FROM Usuario", Usuario.class).getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+
+    public static void atualizarFoto(Integer id, byte[] foto) {
+        EntityManager em = ConnectionFactory.getConnection();
+        try {
+            em.getTransaction().begin();
+            Usuario u = em.find(Usuario.class, id);
+            u.setFotoPerfil(foto);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+
 }
