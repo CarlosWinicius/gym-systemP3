2964950de690b4208b8eedad7b9b9c4df274a4e3
Eduarda <eduarda.damacena@upe.br>
2025-12-12 04:15:48 -0300

    Correções das permissões de administrador

--- src/main/java/br/upe/controller/ui/Main.java
+++ src/main/java/br/upe/controller/ui/Main.java
@@ -1,5 +1,6 @@
 package br.upe.controller.ui;
 
+import br.upe.controller.business.UsuarioService;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -7,14 +8,25 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(LoginScreenController.class.getResource("/ui/LoginScreen.fxml"));
+
+        UsuarioService usuarioService = new UsuarioService();
+        usuarioService.verificarECriarAdminPadrao();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/ui/LoginScreen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+
         stage.setTitle("SYSFIT");
-        stage.getIcons().add(new Image("/images/halter.png"));
+
+        InputStream iconStream = getClass().getResourceAsStream("/images/halter.png");
+        if (iconStream != null) {
+            stage.getIcons().add(new Image(iconStream));
+        }
+
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
